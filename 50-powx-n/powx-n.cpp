88241ceca77c4ac5ccc6df7ec0f977d88bbef126
Vivekpal9957 <vivekpal43697@gmail.com>
2026-08-05 18:08:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (12.89%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    double solve(double x, long n){
+    double solve(double y, long n){
         if(n == 0) return 1;
-        if(n<0){
-           return  solve(1/x, -n);
+        if(n<0) return solve(1/y, -n);
+        if(n % 2 == 0){
+            return solve(y*y, n/2);
+
+        }else{
+            return y * solve(y * y , (n-1)/2);
         }
-        if(n % 2 == 0)
-             return   solve(x*x, n/2);
-        else{
-          return  x *  solve(x*x, (n-1)/2);
-        }
-    }
-    double myPow(double x, int n) {
-        return solve(x, (long)n);
+
+    }    
+    double myPow(double y, int n) {
+      return   solve(y, (long) n);
     }
 };
