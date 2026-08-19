e15b3cac77d228d0c4bc8b0d7f7c90c655921a61
Vivekpal9957 <vivekpal43697@gmail.com>
2026-08-19 19:58:08 +0530

    Time: 13 ms (19.99%) | Memory: 14.4 MB (18.25%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -3,27 +3,24 @@ public:
     string removeDuplicates(string s) {
         string ans;
         stack<char> st;
-
-        for (int i = 0; i < s.size(); i++) {
-            if (st.empty()) {
+        for(int i=0; i<s.size(); i++){
+            if(st.empty()){
                 st.push(s[i]);
                 continue;
             }
-
-            if (st.top() == s[i]) {
+            if(st.top() == s[i]){
                 st.pop();
                 continue;
             }
-
             st.push(s[i]);
         }
-
-        while (!st.empty()) {
+        while(!st.empty()){
             ans.push_back(st.top());
             st.pop();
         }
-
         reverse(ans.begin(), ans.end());
         return ans;
+
+        
     }
 };
